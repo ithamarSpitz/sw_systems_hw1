9f8361ce415133decd5f41e2b90e94091617c9dd
ithamarSpitz <itas12@gmail.com>
2021-11-11 20:27:24 +0200

    fix

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -2,7 +2,27 @@
 #include "NumClass.h"
 #include <math.h>
 
-int reversed(int num){
+int checkSize(int num){
+    if(num>0){
+        return checkSize(num/10)+1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int pali(int num){
+    if(num<10)
+        return 1;
+    int size = checkSize(num);
+    if (num / pow(10, size-1) == num % 10)
+        return pali((num % pow(10, size-1)) / 10);
+    else
+        return 0;
+
+}
+
+/*int reversed(int num){
     int remain;
     int sum=0;
     if(num!=0){
@@ -14,22 +34,14 @@ int reversed(int num){
             return sum;
     return sum;
 }
+*/
 
 int isPalindrome(int num){
-    if(num == reversed(num))
+    if(pali(num))
     {
         return 1;
     }
     return 0;
-}
-
-int checkSize(int num){
-    if(num>0){
-        return checkSize(num/10)+1;
-    }
-    else {
-        return 0;
-    }
 }
 
 int armstrongNumber(int num, int power){  
