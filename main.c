16f2746e74d05ce65c1e3d63c3335c82132f71d5
ithamarSpitz <itas12@gmail.com>
2021-11-11 17:08:15 +0200

    main

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+int main()
+{
+    int first, last;
+    printf("Enter first number: ");
+    scanf("%d", &first);
+    printf("Enter second number: ");
+    scanf("%d", &second);
+    int temp = first;
+    while(temp<=second){
+        if(isPrime(temp)) {
+            printf("Prime Numbers:");
+            printf("%d",temp);}
+        temp++;
+    }
+    temp=first;
+        while(temp<=second){
+        if(isPalindrome(temp)) {
+            printf("\nPalindrome Numbers:");
+            printf("%d",temp);}
+        temp++;
+    }
+    temp=first;
+    while(temp<=second){
+        if(isStrong(temp)) {
+            printf("\nStrong Numbers:");
+            printf("%d",temp);}
+        temp++;
+    }
+    temp=first;
+    while(temp<=second){
+        if(isArmstrong(temp)) {
+            printf("\nArmstrong Numbers:");
+            printf("%d",temp);}
+        temp++;
+    }
+
+}
