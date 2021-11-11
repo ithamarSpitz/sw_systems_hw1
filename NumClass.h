278f4215d6f52eef83fe2c990e7176b23cf7b68b
ithamarSpitz <itas12@gmail.com>
2021-11-11 18:36:50 +0200

    fix

--- NumClass.h
+++ NumClass.h
@@ -1,6 +1,10 @@
+#ifndef NUM_CLASS_H
+#define NUM_CLASS_H
 #include <stdio.h>
 
 int isArmstrong(int);
 int isPalindrome(int);
 int isPrime(int);
-int isStrong(int); 
+int isStrong(int);
+
+#endif /** NUM_CLASS_H */
