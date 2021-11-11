6995d9b5b98a2d33a76ce89250f5004bf73404d5
ithamarSpitz <itas12@gmail.com>
2021-11-11 20:02:30 +0200

    fix

--- main.c
+++ main.c
@@ -11,28 +11,27 @@ int main()
     int temp = first;
     while(temp<=second){
         if(isPrime(temp)) {
-            printf("Prime Numbers:");
             printf("%d",temp);}
         temp++;
     }
     temp=first;
+    printf("/n");
         while(temp<=second){
         if(isPalindrome(temp)) {
-            printf("\nPalindrome Numbers:");
             printf("%d",temp);}
         temp++;
     }
     temp=first;
+    printf("/n");
     while(temp<=second){
         if(isStrong(temp)) {
-            printf("\nStrong Numbers:");
             printf("%d",temp);}
         temp++;
     }
     temp=first;
+    printf("/n");
     while(temp<=second){
         if(isArmstrong(temp)) {
-            printf("\nArmstrong Numbers:");
             printf("%d",temp);}
         temp++;
     }
